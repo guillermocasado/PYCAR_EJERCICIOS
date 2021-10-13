8250e58a1b9ccf988947425dcbdca739ace51ed8
Alejandro Abad <aabadg1@alumnos.nebrija.es>
2021-10-13 16:18:45 +0200

    Initial commit

---
+++ pycar-main/main.py
@@ -0,0 +1,10 @@
+# Import a library of functions called 'pygame'
+import pygame
+from main_functions import *
+
+# Initialize the game engine
+pygame.init()
+
+#call main routine
+size = [1300, 600] # Define size of windows
+main2(size)
